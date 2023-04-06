f95b940428419ed41a367537aae524ae35de2a60
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2023-04-05 21:14:54 -0400

    Move 11ty files into _layouts

--- .eleventy.js
+++ .eleventy.js
@@ -4,4 +4,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
+  return {
+    input: "_layouts",
+    output: "_site",
+  };
 };
