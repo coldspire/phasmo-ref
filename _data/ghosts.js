1944c86e6e02ba8257ede4afa7eceb809f8cfb11
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2023-04-13 21:47:11 -0400

    Change ghost "uniqueInfo" to "notes" for consistency

--- _data/ghosts.js
+++ _data/ghosts.js
@@ -39,14 +39,14 @@ const Evidence = {
 
 /**
  * The master list of ghosts.
- * @type { {name: string, huntSanityThreshold: (number|string[]), evidence: Evidence[], uniqueInfo: string[] }[] }
+ * @type { {name: string, huntSanityThreshold: (number|string[]), evidence: Evidence[], notes: string[] }[] }
  */
 const ghosts = [
   {
     name: "banshee",
     huntSanityThreshold: 50,
     evidence: [Evidence.Dots, Evidence.Fingerprints, Evidence.Orb],
-    uniqueInfo: [
+    notes: [
       "Using a parabolic mic has a 33%-chance of hearing a unique Banshee shriek",
       "Targets one player at a time; will hunt that player, and sanity-check applies only to target (not average)",
       "Performs more singing events than normal",
@@ -59,7 +59,7 @@ const ghosts = [
       "Can use ability to hunt regardless of sanity threshold",
     ],
     evidence: [Evidence.Fingerprints, Evidence.Freezing, Evidence.Writing],
-    uniqueInfo: [
+    notes: [
       "Below-sanity-threshold hunts can still occur and are separate from ability-triggered hunts",
       "Cooldown time between hunts is 20 seconds, instead of the normal 25 seconds",
       "Smudging a Demon prevents hunts for only 60 seconds, instead of the normal 90 seconds",
@@ -70,7 +70,7 @@ const ghosts = [
     name: "deogen",
     huntSanityThreshold: 40,
     evidence: [Evidence.Dots, Evidence.SpiritBox, Evidence.Writing],
-    uniqueInfo: [
+    notes: [
       "Above-average interactivity with DOTS and ghost writing",
       'Spirit Box usage within a meter has 33%-chance of heavy, "bull-like" breathing',
       "During hunt, has line-of-sight to all players and does not need to roam",
@@ -81,7 +81,7 @@ const ghosts = [
     name: "goryo",
     huntSanityThreshold: 50,
     evidence: [Evidence.Dots, Evidence.Emf, Evidence.Fingerprints],
-    uniqueInfo: [
+    notes: [
       "DOTS interaction appears only through a video feed",
       "Doesn't roam far from its favorite room",
       "Cannot change favorite rooms on any difficulty (though the Monkey Paw can force a change)",
@@ -91,7 +91,7 @@ const ghosts = [
     name: "hantu",
     huntSanityThreshold: 50,
     evidence: [Evidence.Fingerprints, Evidence.Freezing, Evidence.Orb],
-    uniqueInfo: [
+    notes: [
       "The colder the room, the faster it moves (so be careful around the favorite room)",
       "Can't turn on the fuse box; twice as likely to turn off the fuse box",
       "When visible and breaker is off, cold breath is visible when Hantu is visible",
@@ -102,7 +102,7 @@ const ghosts = [
     name: "jinn",
     huntSanityThreshold: 50,
     evidence: [Evidence.Emf, Evidence.Fingerprints, Evidence.Freezing],
-    uniqueInfo: [
+    notes: [
       "Cannot turn off a breaker through interactions",
       "Ability lowers sanity of players in same room/3-meter distance by 25%; fuse box will give EMF 2/5",
       "During hunt, very fast if all conditions are met: fuse box is on, Jinn has line-of-sight, and is further than 3 meters away",
@@ -115,7 +115,7 @@ const ghosts = [
       "40% if lights on in ghost's current room",
     ],
     evidence: [Evidence.Writing, Evidence.Orb, Evidence.SpiritBox],
-    uniqueInfo: [
+    notes: [
       "More likely to turn off lights; cannot turn lights on",
       "Ability: May turn off a light (or television, or computer) almost immediately if a light is turned on within 4 meters (cooldown: 10 seconds)",
       "When roaming, prefers unlit rooms",
@@ -126,7 +126,7 @@ const ghosts = [
     name: "moroi",
     huntSanityThreshold: 50,
     evidence: [Evidence.Freezing, Evidence.SpiritBox, Evidence.Writing],
-    uniqueInfo: [
+    notes: [
       "Ability: Curses a player through Spirit Box interaction or parabolic-mic whispers",
       "A cursed player when not in a lit area (either by candle or lamp) has increased passive sanity drain. Sanity pills cure the curse",
       "During a hunt, a Moroi's speed is a function of how low the target's sanity is",
@@ -137,7 +137,7 @@ const ghosts = [
     name: "myling",
     huntSanityThreshold: 50,
     evidence: [Evidence.Emf, Evidence.Fingerprints, Evidence.Writing],
-    uniqueInfo: [
+    notes: [
       "Produces more whispers when using the parabolic mic",
       "Quieter during a hunt -- footsteps will be audible only within the range where electrical malfunctions too (12 meters)",
     ],
@@ -146,7 +146,7 @@ const ghosts = [
     name: "obake",
     huntSanityThreshold: 50,
     evidence: [Evidence.Emf, Evidence.Fingerprints, Evidence.Orb],
-    uniqueInfo: [
+    notes: [
       "Less chance of fingerprints (75% instead of 100%)",
       "May leave unique fingerprints: six-fingered handprint; two fingers on a light switch; five fingers on a keyboard and prison cell door",
       "Can use ability to cut fingerprint-duration time in half. Can be used several times in succession",
@@ -158,7 +158,7 @@ const ghosts = [
     name: "oni",
     huntSanityThreshold: 50,
     evidence: [Evidence.Dots, Evidence.Emf, Evidence.Freezing],
-    uniqueInfo: [
+    notes: [
       "Causes more interactions than normal and more when people are in the ghost room",
       "During an event: won't send an airball; prefers showing full form during event; causes double sanity drain on player collision",
       "During a hunt, is more visible (flickers out less) than other ghosts",
@@ -171,7 +171,7 @@ const ghosts = [
       "Every third flame blown out (can be prevented by normal methods, e.g. a crucifix)",
     ],
     evidence: [Evidence.Freezing, Evidence.Orb, Evidence.SpiritBox],
-    uniqueInfo: [
+    notes: [
       "Can't hunt until all flames in the area (candle, flashlight, campfires) are out",
       "When attempting to hunt and a flame is in the area, the flame is blown out and the hunt fails",
       "Under 60% sanity, the frequency of blown-out flames increases due to the ghost attempting to hunt",
@@ -183,7 +183,7 @@ const ghosts = [
     name: "phantom",
     huntSanityThreshold: 50,
     evidence: [Evidence.Dots, Evidence.Fingerprints, Evidence.SpiritBox],
-    uniqueInfo: [
+    notes: [
       "If photo of ghost is taken during a hunt, the ghost will be invisible in the photo",
       "If photo is taken during an event: ghost will disappear, electronics work again, heartbeat sound stops, but event sound continues",
       "Ability: May pick a player and teleport to it, instead of a normal roam (causes an EMF-2)",
@@ -195,7 +195,7 @@ const ghosts = [
     name: "poltergeist",
     huntSanityThreshold: 50,
     evidence: [Evidence.Fingerprints, Evidence.Writing, Evidence.SpiritBox],
-    uniqueInfo: [
+    notes: [
       "Throws items more often and with more force",
       "Ability allows it to throw multiple items at once (EMF-2; items are EMF-3). Causes sanity drain equal to number of items thrown times two",
       "During a hunt, throws an item every 0.5 seconds",
@@ -208,7 +208,7 @@ const ghosts = [
       "65% when near at least one piece of electronic equipment",
     ],
     evidence: [Evidence.Dots, Evidence.Emf, Evidence.Orb],
-    uniqueInfo: [
+    notes: [
       "During hunt or event, interferes with electronics at greater distance (15m, instead of 10m)",
       "During a hunt, being within 6m/8m/10m (for small/med/large maps) of electronic equipment causes extra speed (2.5m/s)",
       "The following do not affect a Raiju: head-mounted cameras; video/photo cameras if thrown (not placed); motion/sound sensors and DOTS if thrown; inventory objects (unless flashlights are on); anything electrical not brought from the van",
@@ -218,7 +218,7 @@ const ghosts = [
     name: "revenant",
     huntSanityThreshold: 50,
     evidence: [Evidence.Freezing, Evidence.Orb, Evidence.Writing],
-    uniqueInfo: [
+    notes: [
       "During a hunt, moves very slowly (1m/s) when no players detected",
       "During a hunt, moves twice as fast (3m/s) when a player is detected and is near-impossible to run from",
       "The Revenant moves fast until it reaches the point where it last saw a player",
@@ -228,7 +228,7 @@ const ghosts = [
     name: "shade",
     huntSanityThreshold: ["35% normally", "Never if any players in ghost room"],
     evidence: [Evidence.Emf, Evidence.Freezing, Evidence.Writing],
-    uniqueInfo: [
+    notes: [
       "Less likely to do interactions",
       "A player in the room prevents interactions and hunts, regardless of sanity",
       "Smaller average sanity means more likely to perform events",
@@ -240,7 +240,7 @@ const ghosts = [
     name: "spirit",
     huntSanityThreshold: 50,
     evidence: [Evidence.Emf, Evidence.SpiritBox, Evidence.Writing],
-    uniqueInfo: [
+    notes: [
       "When smudged, a Spirit won't start a non-cursed hunt for 3 minutes (instead of the normal 90 seconds)",
       "",
     ],
@@ -252,7 +252,7 @@ const ghosts = [
       "Reduces by -6% per year aged",
     ],
     evidence: [Evidence.Dots, Evidence.Orb, Evidence.Writing],
-    uniqueInfo: [
+    notes: [
       `Appears on ${Evidence.Dots.shortName} and ${Evidence.Writing.shortName} more often`,
       "Ability attempts to age every 1-2 minutes; if a player is in the room, the Thaye will age; else it waits another 30 seconds to try again",
       "Can age up to ten times per contract",
@@ -270,7 +270,7 @@ const ghosts = [
       Evidence.SpiritBox,
       Evidence.FakeOrb,
     ],
-    uniqueInfo: [
+    notes: [
       "Mimics: interaction/event rates and preferences, hunt thresholds and movement speeds, and abilities",
       "If the mimicked ghost has features that modify the same evidence used to find a Mimic, the Mimic will copy the feature (e.g. the Obake's six-fingered handprint)",
       "Changes mimicked ghost type every 30-120 seconds, but not during hunts",
@@ -281,7 +281,7 @@ const ghosts = [
     name: "the twins",
     huntSanityThreshold: 50,
     evidence: [Evidence.Emf, Evidence.Freezing, Evidence.SpiritBox],
-    uniqueInfo: [
+    notes: [
       "Ability allow it to perform two interactions within 16 meters (25% of EMF-5), which can make it difficult to tell the favorite room",
       "When initiating hunt, 50% chance to start at current location (with lower movement speed), and 50% chance to start near the last interaction (faster speed)",
       "Despite the name, two hunts cannot start simultaneously",
@@ -292,7 +292,7 @@ const ghosts = [
     name: "wraith",
     huntSanityThreshold: 50,
     evidence: [Evidence.Dots, Evidence.Emf, Evidence.SpiritBox],
-    uniqueInfo: [
+    notes: [
       "Never steps in salt or leaves UV footprints, though its appearance doesn't float and it still makes footstep sounds",
       "When not hunting, its ability gives it a chance to teleport near a player (EMF-2; 25% change of EMF-5) before continuing normal behavior",
       "Placing salt in a summoning circle and summoning the ghost is a good way to spot (or rule out) a Wraith",
@@ -305,7 +305,7 @@ const ghosts = [
       "80% if players talking within 2 meters",
     ],
     evidence: [Evidence.Dots, Evidence.Orb, Evidence.SpiritBox],
-    uniqueInfo: [
+    notes: [
       "When hunting, the radius which it can hear players or equipment is much smaller than normal (2.5 m)",
       "When using the music box, the Yokai needs to be closer to the player holding the box for the event and cursed hunt",
     ],
@@ -314,7 +314,7 @@ const ghosts = [
     name: "yurei",
     huntSanityThreshold: 50,
     evidence: [Evidence.Dots, Evidence.Freezing, Evidence.Orb],
-    uniqueInfo: [
+    notes: [
       "When smudged, returns to favorite room and is trapped for 60 seconds",
       "Ability causes players within 7.5 meters to lose 15% sanity",
       "Same ability also causes a random open door to be closed fully (EMF-2), which means it closes doors a lot more than normal",
