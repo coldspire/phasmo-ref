c3e08de74ceb3415822c93ab09289f75b1178d2a
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2023-05-15 20:57:18 -0400

    Format sanity ghosts better

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,26 @@
 const CleanCSS = require("clean-css");
 
+/**
+ * Turn an array of ghost names into a string of elements with commas between
+ * @param {string[]} ghosts
+ * @return string
+ */
+function formatGhostNamesArray(ghosts) {
+  return ghosts
+    ?.map((ghostName) => {
+      return `<span class="ghost-name">${ghostName}</span>`;
+    })
+    .join(`, `);
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
 
   eleventyConfig.addPassthroughCopy("src/styles");
+
+  eleventyConfig.addFilter("formatGhostNames", formatGhostNamesArray);
 
   return {
     dir: {
