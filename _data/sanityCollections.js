372cf9f8fdfb8b672d3fa95a56fd1c57d8dddc2a
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2023-05-21 20:31:22 -0400

    Add super-sanity information

--- _data/sanityCollections.js
+++ _data/sanityCollections.js
@@ -2,6 +2,8 @@ const ghosts = require("./ghosts");
 
 /**
  * @typedef { { sanityNum: number; ghosts: string[]; } } SanityPair[]
+ * @typedef { { ghost: string; reason: string; } } SuperCondition
+ * @typedef { { sanityNum: number; superConditions: SuperCondition[] } } SuperSanityPairs[]
  */
 
 /**
@@ -35,8 +37,42 @@ function getSanityStartingSortDesc() {
   return sanityPairs.sort().reverse();
 }
 
+function getSanitySuperSortDesc() {
+  const sanityCollection = {};
+
+  ghosts.forEach((ghost) => {
+    if (Array.isArray(ghost.huntConditions.superSanityThresholds)) {
+      const superThresholds = ghost.huntConditions.superSanityThresholds;
+      superThresholds.forEach((threshold) => {
+        const superSanityPair = {
+          ghost: ghost.name,
+          condition: threshold.condition,
+        };
+        if (Array.isArray(sanityCollection[threshold.threshold])) {
+          sanityCollection[threshold.threshold].push(superSanityPair);
+        } else {
+          sanityCollection[threshold.threshold] = [superSanityPair];
+        }
+      });
+    }
+  });
+
+  Object.values(sanityCollection).forEach((superPairs) => {
+    superPairs.sort((pairA, pairB) => {
+      return pairA.ghost.localeCompare(pairB.ghost);
+    });
+  });
+
+  const sanityPairs = Object.entries(sanityCollection).map(
+    ([sanityNum, superPairs]) => ({ sanityNum, superPairs })
+  );
+
+  return sanityPairs.sort().reverse();
+}
+
 const sanityCollections = {
   sanityStartingSortDesc: getSanityStartingSortDesc(),
+  sanitySuperSortDesc: getSanitySuperSortDesc(),
 };
 
 module.exports = sanityCollections;
