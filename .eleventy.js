609d1259ee454e0a0c8e5101cc1f1f241540f864
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2023-06-22 20:59:02 -0400

    Install Lit and Lit-Eleventy library

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,15 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const CleanCSS = require("clean-css");
+const litPlugin = require("@lit-labs/eleventy-plugin-lit");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(pluginWebc);
+  eleventyConfig.addPlugin(litPlugin, {
+    mode: "worker",
+    componentModules: [],
+  });
 
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
