52b37148c46b1b30326ff0d987837315ab32041d
Owen <5332788+coldspire@users.noreply.github.com>
2023-06-29 10:56:35 -0400

    Add ghost data improvements and updated ghost pages (#20)
    * Break up ghost notes
    
    * Add WebC component for ghost notes
    
    * Update ghost.notes usage for broken-up notes
    
    * Add Fandom wiki links to ghosts
    
    * Add must-know tips to ghosts
    
    * Add ghost pronunciation guide

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const CleanCSS = require("clean-css");
 const litPlugin = require("@lit-labs/eleventy-plugin-lit");
 
 const componentsDir = "_components";
+
+function wikiUrl(ghostName, isTextVersion) {
+  return isTextVersion
+    ? `https://www.textise.net/showText.aspx?strURL=https%253A//phasmophobia.fandom.com/wiki/${ghostName}`
+    : `https://phasmophobia.fandom.com/wiki/${ghostName}`;
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
@@ -10,10 +17,13 @@ module.exports = function (eleventyConfig) {
     mode: "worker",
     componentModules: [`${componentsDir}/ghost-link.mjs`],
   });
+  eleventyConfig.addPlugin(pluginWebc);
 
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
+  eleventyConfig.addShortcode("ghostWikiUrl", wikiUrl);
 
   eleventyConfig.addPassthroughCopy("src/styles");
 
@@ -24,6 +34,7 @@ module.exports = function (eleventyConfig) {
       input: "./src",
       data: "../_data",
       includes: "../_includes",
+      components: "../_components/*.webc",
       output: "./dist",
     },
   };
