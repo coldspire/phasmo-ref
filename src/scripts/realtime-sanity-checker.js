aa9fda3744fef9a21b838f9c90f8e6a6438088fe
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2023-08-29 20:10:02 -0400

    Implement sanity filtering

--- src/scripts/realtime-sanity-checker.js
+++ src/scripts/realtime-sanity-checker.js
@@ -1,1 +1,23 @@
-console.log("Realtime checker works");
+function filterGhostsBySanity(playerSanity) {
+  const ghostEls = Array.from(
+    document.getElementsByClassName("ghost-sanity-hook")
+  );
+  ghostEls.forEach((ghostEl) => {
+    const sanityNums = (ghostEl.dataset.sanityList ?? []).split(",");
+    let ghostCanHunt = false;
+    sanityNums.forEach((sanityNum) => {
+      if (+sanityNum >= +playerSanity) {
+        ghostCanHunt = true;
+      }
+    });
+    ghostEl.classList.toggle("can-hunt", ghostCanHunt);
+    ghostEl.classList.toggle("cannot-hunt", !ghostCanHunt);
+  });
+}
+
+document.addEventListener("input", (event) => {
+  if (event.target.id === "selected-sanity") {
+    const playerSanity = event.target.value;
+    filterGhostsBySanity(playerSanity);
+  }
+});
