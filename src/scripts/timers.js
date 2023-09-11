7327d36f1abfa4cab80def6ae1f2b7f79e5f1379
Owen <5332788+coldspire@users.noreply.github.com>
2023-09-10 21:43:30 -0400

    Add timers (#24)
    * Add stub timers page and script file
    
    * Add buggy working timer
    
    * Implement better start and reset
    
    * Add more timers
    
    * Stop interval countdown when counter is reset early
    
    * Add visual cues for timer states
    
    * Polish up timer style
    
    * Add testing timer
    
    * Add expiration wiggle
    
    * Add timer label
    
    * Add simple view
    
    * Add configuration section for options
    
    * Add no-sleep toggle
    
    * Fix Safari issue with countdown ending on "1"
    
    * Timers: Fix simple view
    
    * Timers: Comment out testing timer
    
    * Timers: Add intermediate setup timer

---
+++ src/scripts/timers.js
@@ -0,0 +1,139 @@
+/**
+ * @typedef {{container: HTMLElement; label: HTMLElement; startBtn: HTMLButtonElement; resetBtn: HTMLButtonElement}} TimerEl
+ */
+
+/**
+ * @readonly
+ * @enum {number}
+ */
+const TimerState = {
+  Reset: 0,
+  Running: 1,
+  Expired: 2,
+};
+
+/**
+ *
+ * @param {HTMLElement} labelEl
+ * @param {number} value
+ */
+function updateLabel(labelEl, value) {
+  labelEl.innerText = value.toString();
+}
+
+/**
+ * @param {TimerState} timerState
+ * @param {TimerEl} els
+ */
+function setTimerButtonState(timerState, els) {
+  const isReset = timerState === TimerState.Reset;
+  const isRunning = timerState === TimerState.Running;
+  const isExpired = timerState === TimerState.Expired;
+
+  els.startBtn.disabled = isRunning || isExpired;
+  els.resetBtn.disabled = isReset;
+
+  els.container.classList.toggle("timer--reset", isReset);
+  els.container.classList.toggle("timer--running", isRunning);
+  els.container.classList.toggle("timer--expired", isExpired);
+
+  if (isExpired) {
+    // Necessary to show "0" on expiry in iOS Safari
+    els.label.textContent = "0";
+  }
+}
+
+/**
+ * @param {number} duration
+ * @param {TimerEl }els
+ */
+function countdownFactory(duration, els) {
+  return ((duration) => {
+    const durationInMs = duration * 1000;
+    let countdownValue = duration;
+
+    updateLabel(els.label, countdownValue);
+    setTimerButtonState(TimerState.Running, els);
+    const countdownInterval = setInterval(() => {
+      countdownValue -= 1;
+      updateLabel(els.label, countdownValue);
+    }, 1000);
+
+    const timerTimeout = setTimeout(() => {
+      setTimerButtonState(TimerState.Expired, els);
+      clearInterval(countdownInterval);
+    }, durationInMs);
+
+    const stopTimer = () => {
+      clearInterval(countdownInterval);
+      clearTimeout(timerTimeout);
+      updateLabel(els.label, duration);
+    };
+
+    return {
+      stopTimer,
+    };
+  })(duration);
+}
+
+/**
+ * @param {number} duration
+ * @param {TimerEl} els
+ */
+function setTimerLogic(duration, els) {
+  setTimerButtonState(TimerState.Reset, els);
+  let countdown;
+
+  els.startBtn.addEventListener("click", () => {
+    countdown = countdownFactory(duration, els);
+  });
+
+  els.resetBtn.addEventListener("click", () => {
+    if (countdown) {
+      countdown.stopTimer();
+    }
+    setTimerButtonState(TimerState.Reset, els);
+  });
+}
+
+function setupTimer(timerEl) {
+  const duration = timerEl.dataset.duration;
+
+  /** @type TimerEl **/
+  const els = {
+    container: timerEl,
+    heading: timerEl.children[0],
+    label: timerEl.children[1],
+    startBtn: timerEl.children[2],
+    resetBtn: timerEl.children[3],
+  };
+
+  if (!duration) {
+    els.label.innerText = "Duration missing for this timer.";
+    els.startBtn.hidden = true;
+    els.resetBtn.hidden = true;
+    return;
+  }
+
+  els.label.innerText = duration;
+  setTimerLogic(+duration, els);
+}
+
+function setupAllTimers() {
+  const timers = Array.from(document.getElementsByClassName("timer"));
+  timers.forEach((timerEl) => {
+    setupTimer(timerEl);
+  });
+}
+
+function updateSimpleView({ target } = { target: { checked: false } }) {
+  document
+    .querySelector(".timers")
+    .classList[target.checked ? "add" : "remove"]("simple-view");
+}
+
+setupAllTimers();
+
+document
+  .querySelector("#show-simple-view")
+  .addEventListener("click", updateSimpleView);
